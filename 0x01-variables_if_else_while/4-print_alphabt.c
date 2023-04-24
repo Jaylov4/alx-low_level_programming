8b0614e6edd478cb676c50518935faafb4aada08
Jaylov4 <jello.chijioke@gmail.com>
2023-04-24 03:14:04 -0700

    Task 4 Prints alphabets without E and Q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets in lowercase
+ * followed by a new line, except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
